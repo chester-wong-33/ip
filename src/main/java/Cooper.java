9e4b51ff5edc4b89bad4657e42f5fc52c374a158
chester-wong-33 <chesterwong0903@gmail.com>
2026-08-21 02:13:43 +0800

    Refactored echo command

--- src/main/java/Cooper.java
+++ src/main/java/Cooper.java
@@ -1,26 +1,30 @@
 import java.util.Scanner;
 
 public class Cooper {
+
+    private static final String DASHES = "\t____________________________________________________________\n";
+    private static final String INTRO = "Hello! I'm Cooper.\n\tWhat can I do for you?";
+
+    private static String echo(String s) {
+        return DASHES + '\t' + s + '\n' + DASHES;
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        String dashes = "\t____________________________________________________________\n";
-        String banner = dashes + "\tHello! I'm Cooper.\n\tWhat can I do for you?\n" + dashes;
-
+        String banner = echo(INTRO);
         System.out.println(banner);
 
         String input = scanner.nextLine();
 
         while (!input.toLowerCase().equals("bye")) {
-            String reply = dashes + '\t' + input + '\n' + dashes;
-
+            String reply = echo(input);
             System.out.println(reply);
 
             input = scanner.nextLine();
         }
 
-        String last = dashes + "\tBye. Hope to see you again soon!\n" + dashes;
-
+        String last = echo("Bye. Hope to see you again soon!");
         System.out.println(last);
     }
 }
