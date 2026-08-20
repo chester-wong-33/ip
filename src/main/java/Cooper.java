afc57eec7356755b6ee6e6adef6f064945304e74
chester-wong-33 <chesterwong0903@gmail.com>
2026-08-21 02:05:45 +0800

    Added echo commands

--- src/main/java/Cooper.java
+++ src/main/java/Cooper.java
@@ -1,17 +1,26 @@
+import java.util.Scanner;
+
 public class Cooper {
     public static void main(String[] args) {
-        String banner = """
-        ____________________________________________________________
-        \
-        Hello! I'm Cooper.
-        \
-        What can I do for you?
-        \
-        ____________________________________________________________
-        \
-        Bye. Hope to see you again soon!
-        \
-        ____________________________________________________________""";
+        Scanner scanner = new Scanner(System.in);
+
+        String dashes = "\t____________________________________________________________\n";
+        String banner = dashes + "\tHello! I'm Cooper.\n\tWhat can I do for you?\n" + dashes;
+
         System.out.println(banner);
+
+        String input = scanner.nextLine();
+
+        while (!input.toLowerCase().equals("bye")) {
+            String reply = dashes + '\t' + input + '\n' + dashes;
+
+            System.out.println(reply);
+
+            input = scanner.nextLine();
+        }
+
+        String last = dashes + "\tBye. Hope to see you again soon!\n" + dashes;
+
+        System.out.println(last);
     }
 }
