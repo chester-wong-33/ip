a67f8701ab25c86659852af3958d5fc27d6a5c69
chester-wong-33 <chesterwong0903@gmail.com>
2026-08-21 05:25:11 +0800

    Added exception handling for most cases

--- src/main/java/Cooper.java
+++ src/main/java/Cooper.java
@@ -7,7 +7,7 @@ public class Cooper {
     private static final String DASHES = "\t_*_*_*______________________________________________________\n";
     private static final String INTRO = "Hello! I'm Cooper.\n\tWhat can I do for you?";
 
-    private List<Task> checklist;
+    private final List<Task> checklist;
 
     private static int wordToNum(String numString) {
         int n = numString.length();
@@ -66,135 +66,113 @@ public class Cooper {
         bot.echo(INTRO);
 
         String input;
-        Action cmd = Action.BYE;
+        Action cmd = Action.LIST;
 
         do {
 
-            input = scanner.nextLine();
-            String[] words = input.split(" ");
-            cmd = Action.valueOf(words[0].toUpperCase());
+            try {
 
-            switch(cmd) {
-                case Action.LIST: {
-                    bot.printChecklist();
-                    break;
-                }
-                case Action.MARK: {
-                    if (words.length == 2) {
-                        int listNum = wordToNum(words[1]);
-                        if (listNum != -1) {
-                            if (listNum <= bot.checklist.size()) {
-                                Task currTask = bot.checklist.get(listNum - 1);
-                                currTask.markAsDone();
-                                bot.echo("Nice! I've marked this task as done:\n\t  " + currTask +
-                                        "\n\tCooper would have loved that :)");
-                            } else {
-                                bot.echo("I coulnd't find the task with that index :(");
-                            }
-                            break;
-                        }
+                input = scanner.nextLine();
+                String[] words = input.split(" ");
+                cmd = Action.valueOf(words[0].toUpperCase());
+
+                switch (cmd) {
+                    case Action.LIST: {
+                        bot.printChecklist();
+                        break;
                     }
-                    bot.echo("Invalid syntax :(");
-                    break;
-                }
-                case Action.UNMARK: {
-                    if (words.length == 2) {
-                        int listNum = wordToNum(words[1]);
-                        if (listNum != -1) {
-                            if (listNum <= bot.checklist.size()) {
-                                Task currTask = bot.checklist.get(listNum - 1);
-                                currTask.markAsUndone();
-                                bot.echo("OK, I've marked this task as not done yet:\n\t  " + currTask +
-                                        "\n\tKeep going! :)");
-                            } else {
-                                bot.echo("I coulnd't find the task with that index :(");
+                    case Action.MARK: {
+                        if (words.length == 2) {
+                            int listNum = wordToNum(words[1]);
+                            if (listNum != -1) {
+                                if (listNum <= bot.checklist.size()) {
+                                    Task currTask = bot.checklist.get(listNum - 1);
+                                    currTask.markAsDone();
+                                    bot.echo("Nice! I've marked this task as done:\n\t  " + currTask +
+                                            "\n\tCooper would have loved that :)");
+                                } else {
+                                    bot.echo("I coulnd't find the task with that index :(");
+                                }
+                                break;
                             }
-                            break;
                         }
+                        bot.echo("Invalid syntax :(");
+                        break;
                     }
-                    bot.echo("Invalid syntax :(");
-                    break;
-                }
-                case Action.TODO: {
-                    ToDo newTodo = new ToDo(input.split(" ", 2)[1]);
-                    bot.addTask(newTodo);
-                    break;
-                }
-                case Action.DEADLINE: {
-                    String[] params = input.split(" /by ");
-                    String taskName = params[0].split("deadline ")[1];
-                    String dueDate = params[1];
+                    case Action.UNMARK: {
+                        if (words.length == 2) {
+                            int listNum = wordToNum(words[1]);
+                            if (listNum != -1) {
+                                if (listNum <= bot.checklist.size()) {
+                                    Task currTask = bot.checklist.get(listNum - 1);
+                                    currTask.markAsUndone();
+                                    bot.echo("OK, I've marked this task as not done yet:\n\t  " + currTask +
+                                            "\n\tKeep going! :)");
+                                } else {
+                                    bot.echo("I coulnd't find the task with that index :(");
+                                }
+                                break;
+                            }
+                        }
+                        bot.echo("Invalid syntax :(");
+                        break;
+                    }
+                    case Action.TODO: {
+                        String[] params = input.split(" ");
+                        if (params.length == 1) {
+                            throw new Exception("Cooper notices that your todo is empty. That's impossible!");
+                        }
 
-                    Deadline newDeadline = new Deadline(taskName, dueDate);
-                    bot.addTask(newDeadline);
-                    break;
-                }
-                case Action.EVENT: {
-                    String[] params = input.split(" /from ");
-                    String taskName = params[0].split("event ")[1];
-                    String startDate = params[1].split(" /to ")[0];
-                    String endDate = params[1].split(" /to ")[1];
+                        ToDo newTodo = new ToDo(input.split(" ", 2)[1]);
+                        bot.addTask(newTodo);
+                        break;
+                    }
+                    case Action.DEADLINE: {
+                        String[] params = input.split(" /by ");
+                        if (params.length != 2) {
+                            throw new Exception("Cooper feels a task can only have exactly 1 deadline!");
+                        }
 
-                    Event newEvent = new Event(taskName, startDate, endDate);
-                    bot.addTask(newEvent);
-                    break;
-                }
-                case Action.BYE: {
-                    cmd = Action.BYE;
-                    break;
-                }
-                default:
-                    bot.echo("Cooper doesn't understand this command :(");
+                        if (params[0].trim().equals("deadline")) {
+                            throw new Exception("Cooper can't keep track of tasks with no name!");
+                        }
+                        String taskName = params[0].split("deadline ")[1];
+                        String dueDate = params[1];
 
+                        Deadline newDeadline = new Deadline(taskName, dueDate);
+                        bot.addTask(newDeadline);
+                        break;
+                    }
+                    case Action.EVENT: {
+                        String[] params = input.split(" /from ");
+                        if (params.length != 2) {
+                            throw new Exception("Cooper feels an event must have a title and start date!");
+                        }
+
+                        if (params[0].trim().equals("event")) {
+                            throw new Exception("Cooper thinks we need a title!");
+                        }
+                        String taskName = params[0].split("event ")[1];
+
+                        String startDate = params[1].split(" /to ")[0];
+                        String endDate = params[1].split(" /to ")[1];
+
+                        Event newEvent = new Event(taskName, startDate, endDate);
+                        bot.addTask(newEvent);
+                        break;
+                    }
+                    case Action.BYE: {
+                        cmd = Action.BYE;
+                        break;
+                    }
+                    default:
+                        throw new Exception("Cooper doesn't understand this command :(");
+                }
+
+            } catch (Exception e) {
+                bot.echo(e.getMessage());
             }
         } while (!cmd.equals(Action.BYE));
-
-//        String input = scanner.nextLine();
-//
-//        while (!input.toLowerCase().equals("bye")) {
-//
-//            if (input.toLowerCase().equals("list")) {
-//                bot.printChecklist();
-//            } else {
-//                String[] words = input.split(" ");
-//
-//                if (words.length == 2 && words[0].equals("mark")) {
-//                    int listNum = wordToNum(words[1]);
-//                    if (listNum != -1) {
-//                        if (listNum <= bot.checklist.size()) {
-//                            Task currTask = bot.checklist.get(listNum - 1);
-//                            currTask.markAsDone();
-//                            bot.echo("Nice! I've marked this task as done:\n\t  " + currTask +
-//                                    "\n\tCooper would have loved that :)");
-//                        } else {
-//                            bot.echo("I coulnd't find the task with that index :(");
-//                        }
-//                        input = scanner.nextLine();
-//                        continue;
-//                    }
-//                }
-//
-//                if (words.length == 2 && words[0].equals("unmark")) {
-//                    int listNum = wordToNum(words[1]);
-//                    if (listNum != -1) {
-//                        if (listNum <= bot.checklist.size()) {
-//                            Task currTask = bot.checklist.get(listNum - 1);
-//                            currTask.markAsUndone();
-//                            bot.echo("OK, I've marked this task as not done yet:\n\t  " + currTask +
-//                                    "\n\tKeep going! :)");
-//                        } else {
-//                            bot.echo("I coulnd't find the task with that index :(");
-//                        }
-//                        input = scanner.nextLine();
-//                        continue;
-//                    }
-//                }
-//                bot.addTask(input);
-//
-//                bot.echo("added: " + input);
-//            }
-//            input = scanner.nextLine();
-//        }
 
         bot.echo("Bye. Hope to see you again soon!");
     }
