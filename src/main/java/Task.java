e06475c0a47b13a038381396a52752d38fd39465
chester-wong-33 <chesterwong0903@gmail.com>
2026-08-21 03:25:29 +0800

    Edited Task class to include isDone

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,12 +1,22 @@
 public class Task {
     private final String description;
+    private boolean isDone;
 
     public Task(String description) {
         this.description = description;
+        this.isDone = false;
+    }
+
+    public void markAsDone() {
+        isDone = true;
+    }
+
+    private String getStatusIcon() {
+        return (isDone ? "X" : " ");
     }
 
     @Override
     public String toString() {
-        return this.description;
+        return "[" + this.getStatusIcon() + "] " + this.description;
     }
 }
