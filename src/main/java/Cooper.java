229da9472e4c823f35c6070581a72d33feb3241f
chester-wong-33 <chesterwong0903@gmail.com>
2026-08-27 18:11:03 +0800

    Refactor main driver program to separate functions for commands

--- src/main/java/Cooper.java
+++ src/main/java/Cooper.java
@@ -9,6 +9,22 @@ public class Cooper {
 
     private final List<Task> checklist;
 
+    private static Action parseAction(String input) throws CooperException {
+        String trimmedInput = input.trim();
+
+        if (trimmedInput.isEmpty()) {
+            throw new CooperException("Please enter a command!");
+        }
+
+        String cmd = trimmedInput.split("\\s+", 2)[0];
+
+        try {
+            return Action.valueOf(cmd.toUpperCase());
+        } catch (IllegalArgumentException e) {
+            throw new CooperException("Cooper doesn't understand this command: " + cmd);
+        }
+    }
+
     private static int wordToNum(String numString) {
         int n = numString.length();
         int num = 0;
@@ -41,7 +57,7 @@ public class Cooper {
                 String.format("\n\tNow you have %d %s in the list.", checklist.size(), status));
     }
 
-    private void printChecklist() {
+    private void handleList() {
         if (checklist.isEmpty()) {
             echo("No current tasks!");
             return;
@@ -58,141 +74,174 @@ public class Cooper {
         echo(preamble + s.delete(s.length() - 1, s.length()).toString());
     }
 
-    public static void main(String[] args) {
+    private void handleDelete(String input) throws CooperException {
+        String[] params = input.split(" ");
+        if (params.length != 2) {
+            throw new CooperException("Deleting is serious! Cooper wishes you provided a proper index only.");
+        }
+
+        int idx = wordToNum(params[1]);
+        if (idx <= 0 || idx > checklist.size()) {
+            throw new CooperException("The index isn't valid!");
+        }
+
+        Task toRemove = checklist.get(idx - 1);
+        checklist.remove(idx - 1);
+        String status = (checklist.size() == 1) ? "task" : "tasks";
+
+        echo("Noted. I've removed this task:\n\t  " + toRemove
+                + String.format("\n\tNow you have %d %s in the list.", checklist.size(), status));
+    }
+
+    private void handleMark(String input) throws CooperException {
+        String[] params = input.trim().split(" ");
+
+        if (params.length != 2) {
+            throw new CooperException("Invalid syntax :( "
+                    + "Cooper would like you to follow the format: mark <task-number>");
+        }
+
+        int listNum = wordToNum(params[1]);
+        if (listNum > 0 && listNum <= checklist.size()) {
+            Task currTask = checklist.get(listNum - 1);
+            currTask.markAsDone();
+            echo("Nice! I've marked this task as done:\n\t  " + currTask +
+                    "\n\tCooper would have loved that :)");
+        } else {
+            throw new CooperException("Cooper couldn't find a task with that index :(");
+        }
+    }
+
+    private void handleUnmark(String input) throws CooperException {
+        String[] params = input.trim().split(" ");
+
+        if (params.length != 2) {
+            throw new CooperException("Invalid syntax :( "
+                    + "Cooper would like you to follow the format: unmark <task-number>");
+        }
+
+        int listNum = wordToNum(params[1]);
+        if (listNum > 0 && listNum <= checklist.size()) {
+            Task currTask = checklist.get(listNum - 1);
+            currTask.markAsUndone();
+            echo("OK, I've marked this task as not done yet:\n\t  " + currTask
+                    + "\n\tKeep going! :)");
+        } else {
+            throw new CooperException("Cooper couldn't find the task with that index :(");
+        }
+    }
+
+    private void handleTodo(String input) throws CooperException {
+        String[] params = input.split(" ");
+
+        if (params.length == 1) {
+            throw new CooperException("Cooper notices that your todo is empty. That's impossible!");
+        }
+
+        ToDo newTodo = new ToDo(input.split(" ", 2)[1]);
+        addTask(newTodo);
+    }
+
+    private void handleDeadline(String input) throws CooperException {
+        String[] params = input.split(" /by ");
+
+        if (params.length != 2) {
+            throw new CooperException("Cooper feels a task can only have exactly 1 deadline!");
+        }
+
+        if (params[0].trim().equals("deadline")) {
+            throw new CooperException("Cooper can't keep track of tasks with no name!");
+        }
+
+        String taskName = params[0].split("deadline ")[1];
+        String dueDate = params[1];
+
+        Deadline newDeadline = new Deadline(taskName, dueDate);
+        addTask(newDeadline);
+    }
+
+    private void handleEvent(String input) throws CooperException {
+        String[] params = input.split(" /from ");
+
+        if (params.length != 2) {
+            throw new CooperException("Cooper feels an event must have a title and start date!");
+        }
+
+        if (params[0].trim().equals("event")) {
+            throw new CooperException("Cooper thinks we need a title!");
+        }
+        String taskName = params[0].split("event ")[1];
+
+        String startDate = params[1].split(" /to ")[0];
+        String endDate = params[1].split(" /to ")[1];
+
+        Event newEvent = new Event(taskName, startDate, endDate);
+        addTask(newEvent);
+    }
+
+    private boolean executeCommand(String input) {
+        Action action = parseAction(input);
+
+        switch (action) {
+            case Action.LIST: {
+                handleList();
+                break;
+            }
+            case Action.DELETE: {
+                handleDelete(input);
+                break;
+            }
+            case Action.MARK: {
+                handleMark(input);
+                break;
+            }
+            case Action.UNMARK: {
+                handleUnmark(input);
+                break;
+            }
+            case Action.TODO: {
+                handleTodo(input);
+                break;
+            }
+            case Action.DEADLINE: {
+                handleDeadline(input);
+                break;
+            }
+            case Action.EVENT: {
+                handleEvent(input);
+                break;
+            }
+            case Action.BYE: {
+                return true;
+            }
+            // did not break, since body returns directly
+            default: {
+                throw new CooperException("Cooper doesn't understand this command :(");
+            }
+        }
+        return false;
+    }
+
+    public void run() {
         Scanner scanner = new Scanner(System.in);
+        echo(INTRO);
 
-        Cooper bot = new Cooper();
-
-        bot.echo(INTRO);
-
-        String input;
-        Action cmd = Action.LIST;
-
-        do {
+        while (scanner.hasNextLine()) {
+            String input = scanner.nextLine();
 
             try {
-
-                input = scanner.nextLine();
-                String[] words = input.split(" ");
-                cmd = Action.valueOf(words[0].toUpperCase());
-
-                switch (cmd) {
-                    case Action.LIST: {
-                        bot.printChecklist();
-                        break;
-                    }
-                    case Action.DELETE: {
-                        String[] params = input.split(" ");
-                        if (params.length != 2) {
-                            throw new Exception("Deleting is serious! Cooper wishes you provided a proper index only.");
-                        }
-
-                        int idx = wordToNum(params[1]);
-                        if (idx == -1 || idx > bot.checklist.size()) {
-                            throw new Exception("The index isn't valid!");
-                        }
-
-                        Task toRemove = bot.checklist.get(idx - 1);
-                        bot.checklist.remove(idx - 1);
-                        String status = (bot.checklist.size() == 1) ? "task" : "tasks";
-
-                        bot.echo("Noted. I've removed this task:\n\t  " + toRemove +
-                                String.format("\n\tNow you have %d %s in the list.", bot.checklist.size(), status));
-                        break;
-                    }
-                    case Action.MARK: {
-                        if (words.length == 2) {
-                            int listNum = wordToNum(words[1]);
-                            if (listNum != -1) {
-                                if (listNum <= bot.checklist.size()) {
-                                    Task currTask = bot.checklist.get(listNum - 1);
-                                    currTask.markAsDone();
-                                    bot.echo("Nice! I've marked this task as done:\n\t  " + currTask +
-                                            "\n\tCooper would have loved that :)");
-                                } else {
-                                    bot.echo("I couldn't find the task with that index :(");
-                                }
-                                break;
-                            }
-                        }
-                        bot.echo("Invalid syntax :(");
-                        break;
-                    }
-                    case Action.UNMARK: {
-                        if (words.length == 2) {
-                            int listNum = wordToNum(words[1]);
-                            if (listNum != -1) {
-                                if (listNum <= bot.checklist.size()) {
-                                    Task currTask = bot.checklist.get(listNum - 1);
-                                    currTask.markAsUndone();
-                                    bot.echo("OK, I've marked this task as not done yet:\n\t  " + currTask +
-                                            "\n\tKeep going! :)");
-                                } else {
-                                    bot.echo("I coulnd't find the task with that index :(");
-                                }
-                                break;
-                            }
-                        }
-                        bot.echo("Invalid syntax :(");
-                        break;
-                    }
-                    case Action.TODO: {
-                        String[] params = input.split(" ");
-                        if (params.length == 1) {
-                            throw new Exception("Cooper notices that your todo is empty. That's impossible!");
-                        }
-
-                        ToDo newTodo = new ToDo(input.split(" ", 2)[1]);
-                        bot.addTask(newTodo);
-                        break;
-                    }
-                    case Action.DEADLINE: {
-                        String[] params = input.split(" /by ");
-                        if (params.length != 2) {
-                            throw new Exception("Cooper feels a task can only have exactly 1 deadline!");
-                        }
-
-                        if (params[0].trim().equals("deadline")) {
-                            throw new Exception("Cooper can't keep track of tasks with no name!");
-                        }
-                        String taskName = params[0].split("deadline ")[1];
-                        String dueDate = params[1];
-
-                        Deadline newDeadline = new Deadline(taskName, dueDate);
-                        bot.addTask(newDeadline);
-                        break;
-                    }
-                    case Action.EVENT: {
-                        String[] params = input.split(" /from ");
-                        if (params.length != 2) {
-                            throw new Exception("Cooper feels an event must have a title and start date!");
-                        }
-
-                        if (params[0].trim().equals("event")) {
-                            throw new Exception("Cooper thinks we need a title!");
-                        }
-                        String taskName = params[0].split("event ")[1];
-
-                        String startDate = params[1].split(" /to ")[0];
-                        String endDate = params[1].split(" /to ")[1];
-
-                        Event newEvent = new Event(taskName, startDate, endDate);
-                        bot.addTask(newEvent);
-                        break;
-                    }
-                    case Action.BYE: {
-                        cmd = Action.BYE;
-                        break;
-                    }
-                    default:
-                        throw new Exception("Cooper doesn't understand this command :(");
+                if (executeCommand(input)) {
+                    break;
                 }
-
-            } catch (Exception e) {
-                bot.echo(e.getMessage());
+            } catch (CooperException e) {
+                echo(e.getMessage());
             }
-        } while (!cmd.equals(Action.BYE));
+        }
 
-        bot.echo("Bye. Hope to see you again soon!");
+        echo("Bye. Hope to see you again soon!");
+    }
+
+    public static void main(String[] args) {
+        new Cooper().run();
     }
 }
